ef0b54c62980d320e89f48598cca4ba24d9155cd
Carlos Eduardo da Silva <kaduardo@imd.ufrn.br>
2015-10-08 15:49:18 -0300

    Updates to the code

--- Encryption-BeanClient/ejbModule/br/ufrn/imd/imd0409/encryption/EncryptionLocalBusiness.java
+++ Encryption-BeanClient/ejbModule/br/ufrn/imd/imd0409/encryption/EncryptionLocalBusiness.java
@@ -1,5 +1,8 @@
 package br.ufrn.imd.imd0409.encryption;
 
+import javax.ejb.Local;
+
+@Local
 public interface EncryptionLocalBusiness extends EncryptionCommonBusiness {
 
 }
--- Encryption-BeanClient/ejbModule/br/ufrn/imd/imd0409/encryption/EncryptionRemoteBusiness.java
+++ Encryption-BeanClient/ejbModule/br/ufrn/imd/imd0409/encryption/EncryptionRemoteBusiness.java
@@ -1,5 +1,8 @@
 package br.ufrn.imd.imd0409.encryption;
 
+import javax.ejb.Remote;
+
+@Remote
 public interface EncryptionRemoteBusiness extends EncryptionCommonBusiness {
 
 }
